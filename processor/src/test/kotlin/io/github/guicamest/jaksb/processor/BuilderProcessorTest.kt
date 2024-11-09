97c8319c1e66120fd233fe78d04e5d1237aa002e
guicamest <283778+guicamest@users.noreply.github.com>
2024-11-09 22:34:02 +0100

    chore(BuilderProcessor): should generate builder with one non-config argument and a config argument [skip ci]

--- processor/src/test/kotlin/io/github/guicamest/jaksb/processor/BuilderProcessorTest.kt
+++ processor/src/test/kotlin/io/github/guicamest/jaksb/processor/BuilderProcessorTest.kt
@@ -13,9 +13,94 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+@file:OptIn(ExperimentalCompilerApi::class)
+
 package io.github.guicamest.jaksb.processor
 
+import com.tschuchort.compiletesting.KotlinCompilation
+import com.tschuchort.compiletesting.KotlinCompilation.ExitCode.OK
+import com.tschuchort.compiletesting.SourceFile
+import com.tschuchort.compiletesting.kspSourcesDir
+import com.tschuchort.compiletesting.kspWithCompilation
+import com.tschuchort.compiletesting.symbolProcessorProviders
+import org.assertj.core.api.Assertions.assertThat
+import org.intellij.lang.annotations.Language
+import org.jetbrains.kotlin.compiler.plugin.ExperimentalCompilerApi
 import org.junit.jupiter.api.DisplayName
+import org.junit.jupiter.api.Test
+import java.io.File
 
 @DisplayName("BuilderProcessor should")
-class BuilderProcessorTest
+class BuilderProcessorTest {
+    @Test
+    fun `generate builder with one required property and a configuration parameter`() {
+        assertGeneratedFile(
+            sourceFileName = "Document.java",
+            source = """
+                package a.b.c;
+                import jakarta.xml.bind.annotation.XmlAccessType;
+                import jakarta.xml.bind.annotation.XmlAccessorType;
+                import jakarta.xml.bind.annotation.XmlElement;
+                import jakarta.xml.bind.annotation.XmlType;
+
+                @XmlAccessorType(XmlAccessType.FIELD)
+                @XmlType(name = "Document", propOrder = {
+                    "name"
+                })
+                public class Document {
+
+                    @XmlElement(name = "Name", required = true)
+                    protected String name;
+
+                    @XmlElement(name = "Age")
+                    protected Integer age;
+                }
+            """,
+            expectedGeneratedResultFileName = "Document.kt",
+            expectedGeneratedSource = """
+                package a.b.c
+
+                fun Document(
+                    name: String,
+                    configure: Document.() -> Unit = {}
+                ) = Document().apply {
+                    this.name = name
+                    configure()
+                }
+            """,
+        )
+    }
+}
+
+private fun assertGeneratedFile(
+    sourceFileName: String,
+    @Language("java") source: String,
+    expectedGeneratedResultFileName: String,
+    @Language("kotlin") expectedGeneratedSource: String,
+) {
+    val compilation =
+        KotlinCompilation().apply {
+            inheritClassPath = true
+            kspWithCompilation = true
+
+            sources =
+                listOf(
+                    SourceFile.java(sourceFileName, source),
+                )
+            symbolProcessorProviders =
+                mutableListOf(
+                    BuilderProcessorProvider(),
+                )
+        }
+    assertThat(compilation.compile().exitCode).isEqualTo(OK)
+
+    val generated =
+        File(
+            compilation.kspSourcesDir,
+            "kotlin/$expectedGeneratedResultFileName",
+        )
+    assertThat(generated).exists()
+    assertThat(
+        generated.readText(),
+    ).isEqualTo(expectedGeneratedSource.trimIndent())
+}
