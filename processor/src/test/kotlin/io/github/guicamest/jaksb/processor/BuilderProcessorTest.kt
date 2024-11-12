967df92dcd650eac2a4152ed72800a43c3abbb7a
guicamest <283778+guicamest@users.noreply.github.com>
2024-11-12 23:30:47 +0100

    chore(BuildProcessorTest): Add test for java primitives

--- processor/src/test/kotlin/io/github/guicamest/jaksb/processor/BuilderProcessorTest.kt
+++ processor/src/test/kotlin/io/github/guicamest/jaksb/processor/BuilderProcessorTest.kt
@@ -34,6 +34,7 @@ import org.junit.jupiter.api.DisplayName
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.TestInstance
 import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.Arguments.arguments
 import org.junit.jupiter.params.provider.MethodSource
 import java.io.File
 import java.math.BigDecimal
@@ -95,6 +96,62 @@ class BuilderProcessorTest {
                 import kotlin.Unit
 
                 public fun Document(name: ${propertyType.simpleName}, configure: Document.() -> Unit = {}): Document = Document().apply {
+                    this.name = name
+                    configure()
+                }
+            """,
+        )
+    }
+
+    // https://en.wikipedia.org/wiki/Jakarta_XML_Binding#Default_data_type_bindings
+    private fun primitiveProperties() =
+        Stream.of(
+            arguments("int", Int::class),
+            arguments("long", Long::class),
+            arguments("short", Short::class),
+            arguments("boolean", Boolean::class),
+            arguments("float", Float::class),
+            arguments("double", Double::class),
+            arguments("byte", Byte::class),
+            arguments("byte[]", ByteArray::class),
+        )
+
+    @ParameterizedTest
+    @MethodSource("primitiveProperties")
+    fun `generate builder with one required primitive property and a configuration parameter`(
+        javaPrimitive: String,
+        kotlinType: KClass<*>,
+    ) {
+        assertGeneratedFile(
+            sourceFileName = "Document.java",
+            source = """
+                package a.b.c;
+                import jakarta.xml.bind.annotation.XmlAccessType;
+                import jakarta.xml.bind.annotation.XmlAccessorType;
+                import jakarta.xml.bind.annotation.XmlElement;
+                import jakarta.xml.bind.annotation.XmlType;
+
+                @XmlAccessorType(XmlAccessType.FIELD)
+                @XmlType(name = "Document", propOrder = {
+                    "name"
+                })
+                public class Document {
+
+                    @XmlElement(name = "Name", required = true)
+                    protected $javaPrimitive name;
+
+                    @XmlElement(name = "Age")
+                    protected Integer age;
+                }
+            """,
+            expectedGeneratedResultFileName = "a/b/c/Document.kt",
+            expectedGeneratedSource = """
+                package a.b.c
+
+                import ${kotlinType.qualifiedName}
+                import kotlin.Unit
+
+                public fun Document(name: ${kotlinType.simpleName}, configure: Document.() -> Unit = {}): Document = Document().apply {
                     this.name = name
                     configure()
                 }
